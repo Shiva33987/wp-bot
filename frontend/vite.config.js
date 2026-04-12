3a5010d5428df5815e94b2284658aec82ac521a0
shivaram33987-blip <shivaram33987@gmail.com>
2026-04-13 00:19:07 +0530

    Fix chunk size warnings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'pdf': ['jspdf', 'jspdf-autotable'],
+          'excel': ['xlsx'],
+          'axios': ['axios'],
+        },
+      },
+    },
+  },
   server: {
     port: 5173,
     strictPort: false,
